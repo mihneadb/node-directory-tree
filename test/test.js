f2ee1cdce9dfc0d9e302f4cd88176571f84725e6
Ahmed Abdalla <aabdalla@amplify-nation.com>
2015-05-28 14:04:27 -0400

    edits tests, adds gitignore to ignore DS_Store mac files

--- test/test.js
+++ test/test.js
@@ -4,18 +4,18 @@ var dirtree = require('../lib/directory-tree');
 
 describe('directoryTree', function() {
   it('should return an Object', function() {
-    var tree = dirtree.directoryTree('./test/test_data');
+    var tree = dirtree.directoryTree('./test/test_data', '.DS_Store', '.gitkeep');
     expect(tree).to.be.an('object');
   });
 
   it('should list the children in a directory', function() {
-    var tree = dirtree.directoryTree('./test/test_data', ['.DS_Store',]);
+    var tree = dirtree.directoryTree('./test/test_data', '.DS_Store', '.gitkeep');
     console.log(tree);
     expect(tree.children.length).to.equal(4);
   });
 
   it('should display the size of a directory (summing up the children)', function() {
-    var tree = dirtree.directoryTree('./test/test_data', ['.DS_Store', ]);
-    expect(tree.size).to.equal(23600);
+    var tree = dirtree.directoryTree('./test/test_data', '.DS_Store', '.gitkeep');
+    expect(tree.size).to.equal(40000);
   });
 });
