85f62bc38da219cdf2bb64b386dc567c34d5311d
Ahmed Abdalla <aabdalla@amplify-nation.com>
2015-05-28 14:14:35 -0400

    edits tests

--- test/test.js
+++ test/test.js
@@ -4,18 +4,17 @@ var dirtree = require('../lib/directory-tree');
 
 describe('directoryTree', function() {
   it('should return an Object', function() {
-    var tree = dirtree.directoryTree('./test/test_data', '.DS_Store', '.gitkeep');
+    var tree = dirtree.directoryTree('./test/test_data', ['.DS_Store', '.gitkeep']);
     expect(tree).to.be.an('object');
   });
 
   it('should list the children in a directory', function() {
-    var tree = dirtree.directoryTree('./test/test_data', '.DS_Store', '.gitkeep');
-    console.log(tree);
-    expect(tree.children.length).to.equal(4);
+    var tree = dirtree.directoryTree('./test/test_data', ['.DS_Store', '.gitkeep']);
+    expect(tree.children.length).to.equal(3);
   });
 
   it('should display the size of a directory (summing up the children)', function() {
-    var tree = dirtree.directoryTree('./test/test_data', '.DS_Store', '.gitkeep');
-    expect(tree.size).to.equal(40000);
+    var tree = dirtree.directoryTree('./test/test_data', ['.DS_Store', '.gitkeep']);
+    expect(tree.size).to.equal(11304);
   });
 });
