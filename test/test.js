ef8582e644b60fba9f9a8ea7e4141e49ab1d2e86
Tomasz Borychowski <tborychowski@gmail.com>
2016-04-08 17:02:05 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -4,17 +4,17 @@ var dirtree = require('../lib/directory-tree');
 
 describe('directoryTree', function() {
   it('should return an Object', function() {
-    var tree = dirtree.directoryTree('./test/test_data', ['.DS_Store', '.gitkeep']);
+    var tree = dirtree('./test/test_data', ['.DS_Store', '.gitkeep']);
     expect(tree).to.be.an('object');
   });
 
   it('should list the children in a directory', function() {
-    var tree = dirtree.directoryTree('./test/test_data', ['.DS_Store', '.gitkeep']);
+    var tree = dirtree('./test/test_data', ['.DS_Store', '.gitkeep']);
     expect(tree.children.length).to.equal(3);
   });
 
   it('should display the size of a directory (summing up the children)', function() {
-    var tree = dirtree.directoryTree('./test/test_data', ['.DS_Store', '.gitkeep']);
+    var tree = dirtree('./test/test_data', ['.DS_Store', '.gitkeep']);
     expect(tree.size).to.equal(11304);
   });
 });
