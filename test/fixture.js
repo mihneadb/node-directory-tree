dae9f1bfa45bd8222d8769c821c0858f086dbbc1
Gabriel Delépine <gabriel.delepine@scmfrance.fr>
2017-04-19 10:47:59 +0200

    added an item type

--- test/fixture.js
+++ test/fixture.js
@@ -1,37 +1,44 @@
 tree = {
   "path": "./test/test_data",
   "name": "test_data",
+  "type": "directory",
   "children": [
     {
       "path": "test/test_data/file_a.txt",
       "name": "file_a.txt",
       "size": 12,
+      "type": "file",
       "extension": ".txt"
     },
     {
       "path": "test/test_data/file_b.txt",
       "name": "file_b.txt",
       "size": 3756,
+      "type": "file",
       "extension": ".txt"
     },
     {
       "path": "test/test_data/some_dir",
       "name": "some_dir",
+      "type": "directory",
       "children": [
         {
           "path": "test/test_data/some_dir/another_dir",
           "name": "another_dir",
+          "type": "directory",
           "children": [
             {
               "path": "test/test_data/some_dir/another_dir/file_a.txt",
               "name": "file_a.txt",
               "size": 12,
+              "type": "file",
               "extension": ".txt"
             },
             {
               "path": "test/test_data/some_dir/another_dir/file_b.txt",
               "name": "file_b.txt",
               "size": 3756,
+              "type": "file",
               "extension": ".txt"
             }
           ],
@@ -41,12 +48,14 @@ tree = {
           "path": "test/test_data/some_dir/file_a.txt",
           "name": "file_a.txt",
           "size": 12,
+          "type": "file",
           "extension": ".txt"
         },
         {
           "path": "test/test_data/some_dir/file_b.txt",
           "name": "file_b.txt",
           "size": 3756,
+          "type": "file",
           "extension": ".txt"
         }
       ],
@@ -55,11 +64,13 @@ tree = {
     {
       "path": "test/test_data/some_dir_2",
       "name": "some_dir_2",
+      "type": "directory",
       "children": [
         {
           "path": "test/test_data/some_dir_2/.gitkeep",
           "name": ".gitkeep",
           "size": 0,
+          "type": "file",
           "extension": ""
         }
       ],
