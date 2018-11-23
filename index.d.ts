d5531bdcd0aaf924c712e08b7040546f23535ec9
James Bruno <43601253+meta-exception@users.noreply.github.com>
2018-11-23 23:19:15 +0300

    update declare file

--- index.d.ts
+++ index.d.ts
@@ -3,9 +3,11 @@ declare const directoryTree: (
   options?: {
     normalizePath?: boolean;
     exclude?: RegExp | RegExp[];
+    attributes?: string[];
     extensions?: RegExp;
   },
-  onEachFile?: (item: DirectoryTree, path: string) => void
+  onEachFile?: (item: DirectoryTree, path: string, stats: Stats) => void,
+  onEachDirectory?: (item: DirectoryTree, path: string, stats: Stats) => void,
 ) => DirectoryTree;
 
 export declare class DirectoryTree {
@@ -15,6 +17,34 @@ export declare class DirectoryTree {
   type: "directory" | "file";
   children?: DirectoryTree[];
   extension?: string;
+}
+
+export class Stats {
+  isFile(): boolean;
+  isDirectory(): boolean;
+  isBlockDevice(): boolean;
+  isCharacterDevice(): boolean;
+  isSymbolicLink(): boolean;
+  isFIFO(): boolean;
+  isSocket(): boolean;
+  dev: number;
+  ino: number;
+  mode: number;
+  nlink: number;
+  uid: number;
+  gid: number;
+  rdev: number;
+  size: number;
+  blksize: number;
+  blocks: number;
+  atimeMs: number;
+  mtimeMs: number;
+  ctimeMs: number;
+  birthtimeMs: number;
+  atime: Date;
+  mtime: Date;
+  ctime: Date;
+  birthtime: Date;
 }
 
 export default directoryTree;
