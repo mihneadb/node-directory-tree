a13bb52deda418a0d63ef15d5772a173f3ffaa72
Fchen <mario@fridrichovsky.at>
2019-01-04 11:35:54 +0100

    implementing eslint

--- test/fixtureExclude.js
+++ test/fixtureExclude.js
@@ -1,61 +1,61 @@
-tree = {
-  "path": "./test/test_data",
-  "name": "test_data",
-  "children": [
+const tree = {
+  'path': './test/test_data',
+  'name': 'test_data',
+  'children': [
     {
-      "path": "test/test_data/file_a.txt",
-      "name": "file_a.txt",
-      "size": 12,
-      "extension": ".txt",
-      "type": "file"
+      'path': 'test/test_data/file_a.txt',
+      'name': 'file_a.txt',
+      'size': 12,
+      'extension': '.txt',
+      'type': 'file'
     },
     {
-      "path": "test/test_data/file_b.txt",
-      "name": "file_b.txt",
-      "size": 3756,
-      "extension": ".txt",
-      "type": "file"
+      'path': 'test/test_data/file_b.txt',
+      'name': 'file_b.txt',
+      'size': 3756,
+      'extension': '.txt',
+      'type': 'file'
     },
     {
-      "path": "test/test_data/some_dir",
-      "name": "some_dir",
-      "children": [
+      'path': 'test/test_data/some_dir',
+      'name': 'some_dir',
+      'children': [
         {
-          "path": "test/test_data/some_dir/file_a.txt",
-          "name": "file_a.txt",
-          "size": 12,
-          "extension": ".txt",
-          "type": "file"
+          'path': 'test/test_data/some_dir/file_a.txt',
+          'name': 'file_a.txt',
+          'size': 12,
+          'extension': '.txt',
+          'type': 'file'
         },
         {
-          "path": "test/test_data/some_dir/file_b.txt",
-          "name": "file_b.txt",
-          "size": 3756,
-          "extension": ".txt",
-          "type": "file"
+          'path': 'test/test_data/some_dir/file_b.txt',
+          'name': 'file_b.txt',
+          'size': 3756,
+          'extension': '.txt',
+          'type': 'file'
         }
       ],
-      "type": "directory",
-      "size": 3768
+      'type': 'directory',
+      'size': 3768
     },
     {
-      "path": "test/test_data/some_dir_2",
-      "name": "some_dir_2",
-      "children": [
+      'path': 'test/test_data/some_dir_2',
+      'name': 'some_dir_2',
+      'children': [
         {
-          "path": "test/test_data/some_dir_2/.gitkeep",
-          "name": ".gitkeep",
-          "size": 0,
-          "extension": "",
-          "type": "file"
+          'path': 'test/test_data/some_dir_2/.gitkeep',
+          'name': '.gitkeep',
+          'size': 0,
+          'extension': '',
+          'type': 'file'
         }
       ],
-      "size": 0,
-      "type": "directory"
+      'size': 0,
+      'type': 'directory'
     }
   ],
-  "size": 7536,
-  "type": "directory"
-}
+  'size': 7536,
+  'type': 'directory'
+};
 
 module.exports = tree;
