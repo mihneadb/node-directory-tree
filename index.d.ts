af10d0848cfe3c6ae453804985465be7b15495ba
lukas <lukas.private@gmx.de>
2022-01-30 15:57:50 +0100

    encapsulated additional custom properties to parent field "custom"

--- index.d.ts
+++ index.d.ts
@@ -1,33 +1,37 @@
 import { Stats } from 'fs';
 
 declare function directoryTree(
-  path: string,
-  options ? : directoryTree.DirectoryTreeOptions,
-  onEachFile ? : directoryTree.DirectoryTreeCallback,
-  onEachDirectory ? : directoryTree.DirectoryTreeCallback,
+	path: string,
+	options?: directoryTree.DirectoryTreeOptions,
+	onEachFile?: directoryTree.DirectoryTreeCallback,
+	onEachDirectory?: directoryTree.DirectoryTreeCallback
 ): directoryTree.DirectoryTree;
 
 export as namespace directoryTree;
 
 declare namespace directoryTree {
-  export interface DirectoryTree {
-    path: string;
-    name: string;
-    size: number;
-    type: "directory" | "file";
-    children ? : DirectoryTree[];
-    extension?: string;
-    isSymbolicLink?: boolean;
-    [key: string]: any;
-  }
-  export interface DirectoryTreeOptions {
-    normalizePath ? : boolean;
-    exclude ? : RegExp | RegExp[];
-    attributes ? : (keyof Stats | "type" | "extension")[];
-    extensions ? : RegExp;
-    followSymlink ? : boolean;
-  }
-  export type DirectoryTreeCallback = (item: DirectoryTree, path: string, stats: Stats) => void;
+	export interface DirectoryTree {
+		path: string;
+		name: string;
+		size: number;
+		type: 'directory' | 'file';
+		children?: DirectoryTree[];
+		extension?: string;
+		isSymbolicLink?: boolean;
+		custom: { [key: string]: any };
+	}
+	export interface DirectoryTreeOptions {
+		normalizePath?: boolean;
+		exclude?: RegExp | RegExp[];
+		attributes?: (keyof Stats | 'type' | 'extension')[];
+		extensions?: RegExp;
+		followSymlink?: boolean;
+	}
+	export type DirectoryTreeCallback = (
+		item: DirectoryTree,
+		path: string,
+		stats: Stats
+	) => void;
 }
 
 export = directoryTree;
